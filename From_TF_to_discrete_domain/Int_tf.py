f686b96798e8897d37eb339a25f4a6aa92068f4f
PadiMain <padisons0873@gmail.com>
2022-07-25 22:47:15 +0300

    added comments in Int_tf.py

--- From_TF_to_discrete_domain/Int_tf.py
+++ From_TF_to_discrete_domain/Int_tf.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+"""
+For p (Laplace variable) make a replacement 2/Td * ((z - 1)/(z + 1)) in z domain and then derive the difference equation
+"""
 
 
 def input_fun(x, freq):
@@ -7,7 +10,7 @@ def input_fun(x, freq):
     return 1 * np.sin(in_p)
 
 
-def Intagrate_tf_fun(input, Td, initial_val):
+def integrate_tf_fun(input, Td, initial_val):
     output = [initial_val]
     for n in range(1, len(input)):
         output.append(Td * (input[n] + input[n-1])/2 + output[n-1])
@@ -15,13 +18,13 @@ def Intagrate_tf_fun(input, Td, initial_val):
 
 
 def main():
-    freq = 10
-    Time = 1/freq
-    fd = 1000
-    x = np.arange(0, Time, 1/fd)
-    in_put = input_fun(x, freq)
-    output = Intagrate_tf_fun(in_put, 1/fd, 0)
-    plt.plot(x, output)
+    freq = 10  # Sine frequency
+    time = 1/freq  # Stop simulation time (1 period of signal)
+    fd = 1000  # Sampling frequency
+    x = np.arange(0, time, 1/fd)  # Simulation time
+    in_put = input_fun(x, freq)  # Input of integrator
+    output = integrate_tf_fun(in_put, 1/fd, 0)  # Output of integrator (integration with Initial condition=0)
+    plt.plot(x, output)  # Plot of output
     plt.grid()
     plt.show()
 
