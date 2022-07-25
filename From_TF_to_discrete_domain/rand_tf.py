bb9c03970d49f306af2bc1d86fa6e408e830641d
PadiMain <padisons0873@gmail.com>
2022-07-26 00:07:08 +0300

    added implementation for random transfer function

---
+++ From_TF_to_discrete_domain/rand_tf.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+"""
+For p (Laplace variable) make a replacement 2/Td * ((z - 1)/(z + 1)) in z domain and then derive the difference equation
+             1
+  tf = ---------------
+       0.5 s^2 + s + 1
+       
+Input = step from 1 s
+"""
+
+
+def step(x, step_time):
+    in_p = [1 if n >= step_time else 0 for n in x]
+    return np.array(in_p)
+
+
+def rand_tf_fun(input, initial_val):
+    output = initial_val
+    for n in range(2, len(input)):
+        output.append(0.00935 * input[n-1] + 0.008747 * input[n-2] + 1.801 * output[n-1] - 0.8187 * output[n-2])
+    return np.array(output)
+
+
+def main():
+    time = 10  # Stop simulation time
+    fd = 10  # Sampling frequency
+    x = np.arange(0, time, 1/fd)  # Simulation time
+    in_put = step(x, 1)  # Input of tf
+    output = rand_tf_fun(in_put, [0, 0])  # Output of tf (Initial condition=0, 0)
+    plt.plot(x, output)  # Plot of output
+    plt.grid()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
+
